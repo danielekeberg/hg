3ad39f6a4832ca9b01fdf960b8b01750ae65bc95
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-12 17:01:59 +0100

    Update Vendors.tsx

--- app/components/Vendors.tsx
+++ app/components/Vendors.tsx
@@ -7,41 +7,37 @@ export default function Vendors() {
                     <p>Vi samarbeider med kvalitetsleverandører av gravsteiner i forskjellige materialer og stiler</p>
                 </div>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-10 text-center">
                 <div>
                     <div className="overflow-hidden bg-white min-h-100 flex items-end border border-neutral-300 rounded-[2]">
-                        <img src="https://eidestein.no/img/modeller/mod-750-jpg/375A.jpg" className="hover:scale-102 transition duration-500" />
+                        <img src="https://eidestein.no/img/modeller/mod-750-jpg/375A.jpg" alt="Gravstein modell 375A" className="hover:scale-102 transition duration-500" />
                     </div>
                     <div className="p-2">
-                        <h1 className="text-[#444] text-xl mb-2">Tradisjonell gravstein</h1>
-                        <p className="text-sm">Klassisk design i polert mørk granitt med elegant kurvet topp. Tidløs og verdig</p>
+                        <h1 className="text-[#444] text-xl mb-2">Modell 375a</h1>
                     </div>
                 </div>
                 <div>
                     <div className="overflow-hidden bg-white min-h-100 flex items-end border border-neutral-300 rounded-[2]">
-                        <img src="https://eidestein.no/img/modeller/mod-750-jpg/366-B.jpg" className="hover:scale-102 transition duration-500" />
+                        <img src="https://eidestein.no/img/modeller/mod-750-jpg/366-B.jpg" alt="Gravstein modell 366B" className="hover:scale-102 transition duration-500" />
                     </div>
                     <div className="p-2">
-                        <h1 className="text-[#444] text-xl mb-2">Tradisjonell gravstein</h1>
-                        <p className="text-sm">Klassisk design i polert mørk granitt med elegant kurvet topp. Tidløs og verdig</p>
+                        <h1 className="text-[#444] text-xl mb-2">Modell 366b</h1>
                     </div>
                 </div>
                 <div>
                     <div className="overflow-hidden bg-white min-h-100 flex items-end border border-neutral-300 rounded-[2]">
-                        <img src="https://www.nergaard.no/uploads/_CGSmartImage/img-9e38aa4cd3c92622cc5ebb0e7bd6741f" className="hover:scale-102 transition duration-500" />
+                        <img src="https://www.nergaard.no/uploads/_CGSmartImage/img-9e38aa4cd3c92622cc5ebb0e7bd6741f" alt="Gravstein modell 430" className="hover:scale-102 transition duration-500" />
                     </div>
                     <div className="p-2">
-                        <h1 className="text-[#444] text-xl mb-2">Tradisjonell gravstein</h1>
-                        <p className="text-sm">Klassisk design i polert mørk granitt med elegant kurvet topp. Tidløs og verdig</p>
+                        <h1 className="text-[#444] text-xl mb-2">Modell 430</h1>
                     </div>
                 </div>
                 <div>
                     <div className="overflow-hidden bg-white min-h-100 flex items-end border border-neutral-300 rounded-[2]">
-                        <img src="https://www.nergaard.no/uploads/_CGSmartImage/img-dce4bfc538208d4662d8957595df1536" className="hover:scale-102 transition duration-500" />
+                        <img src="https://www.nergaard.no/uploads/_CGSmartImage/img-dce4bfc538208d4662d8957595df1536" alt="Gravstein modell 454" className="hover:scale-102 transition duration-500" />
                     </div>
                     <div className="p-2">
-                        <h1 className="text-[#444] text-xl mb-2">Tradisjonell gravstein</h1>
-                        <p className="text-sm">Klassisk design i polert mørk granitt med elegant kurvet topp. Tidløs og verdig</p>
+                        <h1 className="text-[#444] text-xl mb-2">Modell 454</h1>
                     </div>
                 </div>
             </div>
