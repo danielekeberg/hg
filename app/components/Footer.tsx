1faa3d59c0f98c3c227f803bd314d334cb04bcab
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-12 17:01:41 +0100

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const now = new Date();
     const year = now.getFullYear();
     return(
-        <div className="px-5 md:px-[15%] bg-[#e7e1da] pt-15 pb-5">
+        <div className="px-5 md:px-[15%] pt-15 pb-5">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
                 <div className="flex flex-col gap-2">
                     <h3 className="text-[#444] text-xl">Hadeland Gravstein</h3>
@@ -12,11 +12,11 @@ export default function Footer() {
                     <h3 className="text-[#444] text-xl">Kontakt</h3>
                     <div className="flex gap-2">
                         <img src="/phone.svg" className="w-5 h-5" />
-                        <p className="text-sm">+47 123 45 678</p>
+                        <p className="text-sm">+47 61 33 20 30</p>
                     </div>
                     <div className="flex gap-2">
                         <img src="/email.svg" className="w-5 h-5" />
-                        <p className="text-sm">post@hadelandgravstein.no</p>
+                        <p className="text-sm">haavard.ekeberg@jolstad.no</p>
                     </div>
                     <div className="flex gap-2">
                         <img src="/location.svg" className="w-5 h-5" />
@@ -26,8 +26,6 @@ export default function Footer() {
                 <div className="flex flex-col gap-2">
                     <h3 className="text-[#444] text-xl">Åpningstider</h3>
                     <p className="text-sm">Mandag - Fredag: 09:00 - 16:00</p>
-                    <p className="text-sm">Lørdag: 10:00 - 14:00</p>
-                    <p className="text-sm">Søndag: Stengt</p>
                     <p className="text-sm">Eller etter avtale</p>
                 </div>
                 
