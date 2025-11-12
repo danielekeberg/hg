7f222d389d6f7616c7fb4f4f84dd9e8cc581de84
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-12 15:38:33 +0100

    Create contact/page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/components/Header";
+import Contact from "@/app/components/Contact";
+import Footer from "@/app/components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
