3aa1dcc8437d9d43448d73aa2296f850c5a2fc2b
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-14 19:36:31 +0100

    Update ContactCard.tsx

--- app/components/ContactCard.tsx
+++ app/components/ContactCard.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 export default function ContactCart() {
     return(
-        <div className="bg-stone-500 mt-20 text-white px-5 py-15 flex justify-center">
-            <div className="w-150 text-center flex flex-col gap-5">
+        <div className="bg-stone-500 text-white px-5 py-15 flex justify-center">
+            <div className="w-100 text-center flex flex-col gap-5">
                 <h1 className="text-4xl">La oss hjelpe deg</h1>
                 <p>Ta kontakt for en uforpliktende samtale. Vi er her for å veilede deg gjennom hele prosessen.</p>
                 <div>
-                    <Link href="../contact/" className="bg-[#e7e1da] hover:bg-[#e7e1da]/80 py-2 px-5 text-[#444] rounded-[2] cursor-pointer">Kontakt oss i dag</Link>
+                    <Link href="../kontakt/" className="py-2 px-5 rounded-[2] bg-[#e7e1da] text-[#444] hover:bg-[#e7e1da]/80">Kontakt oss i dag</Link>
                 </div>
             </div>
         </div>
