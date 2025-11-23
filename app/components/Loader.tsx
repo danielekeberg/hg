9907ce190efa86bcb6f7db90ed5792aec26f3c54
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-23 23:34:10 +0100

    Create Loader

---
+++ app/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return(
+        <div className="flex items-center justify-center block">
+            <div className="w-10 h-10 border-5 border-t-transparent border-neutral-500 rounded-full animate-spin" />
+        </div>
+    )
+}
