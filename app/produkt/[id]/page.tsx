b5fd1c5dfb12e9a6e22197e09e8c1c02f37082a1
Daniel Lehre Ekeberg <danieleke@live.no>
2025-11-26 15:58:34 +0100

    Create Product page

---
+++ app/produkt/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/app/components/Header";
+import Product from "@/app/components/Product";
+import Footer from "@/app/components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Product />
+      <Footer />
+    </div>
+  );
+}
